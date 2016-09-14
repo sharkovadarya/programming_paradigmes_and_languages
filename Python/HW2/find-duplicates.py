98083ee9df1436a3ffae4769315b1f5d99a3668e
Darya <sharkovadarya@gmail.com>
2016-09-14 21:11:03 +0300

    Added h/2 #2: find file duplicates using hash functions

---
+++ Python/HW2/find-duplicates.py
@@ -0,0 +1,27 @@
+import sys, os, hashlib
+
+def md5(path):
+    digest = hashlib.md5()
+    with open(path, "rb") as f:
+        digest.update(f.read())
+    return digest.hexdigest()
+
+def find_duplicates(path):
+    # Using a dictionary provides searching in O(1)
+    # Store hashes as keys and lists of pathes as values
+    from collections import defaultdict
+    duplicates = defaultdict(list)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if not file.startswith(".") and not file.startswith("~"):
+                file_path = os.path.join(root, file)
+                file_hash = md5(file_path)
+                if file_hash in duplicates:
+                    duplicates[file_hash].append(file_path)
+                else:
+                    duplicates[file_hash] = [file_path]
+
+    for file_hash in duplicates:
+        if len(duplicates[file_hash]) > 1:
+            print(':'.join(duplicates[file_hash]))
+
