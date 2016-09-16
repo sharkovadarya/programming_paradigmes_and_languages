f2a0c8680387bc24a9026fb20d237ff803a99161
Darya <sharkovadarya@gmail.com>
2016-09-16 12:01:06 +0300

    quick fix

--- Python/HW2/find-duplicates.py
+++ Python/HW2/find-duplicates.py
@@ -3,33 +3,28 @@ import os
 import hashlib
 
 
-def read_file(path):
-    file = b''
-    with open(path, "rb") as f:
-        file += f.read(1024)
-    return file
-
-
-def md5(file):
+def md5(path):
     digest = hashlib.md5()
-    digest.update(file)
+    with open(path, "rb") as f:
+        for block in iter(lambda: f.read(1024), b''):
+            digest.update(block)
     return digest.hexdigest()
 
 
 def find_duplicates(path):
     # Using a dictionary provides searching in O(1)
     # Store hashes as keys and lists of paths as values
-    duplicates = {}
+    from collections import defaultdict
+    duplicates = defaultdict(list)
     for root, _, files in os.walk(path):
         for file in files:
-            if not file.startswith(".") and not file.startswith("~"):
-                file_path = os.path.join(root, file)
-                if not os.path.islink(file_path):
-                    file_hash = md5(read_file(file_path))
-                    if file_hash in duplicates:
-                        duplicates[file_hash].append(file_path)
-                    else:
-                        duplicates[file_hash] = [file_path]
+            if file.startswith(".") or file.startswith("~"):
+                continue
+            file_path = os.path.join(root, file)
+            if os.path.islink(file_path):
+                continue
+            file_hash = md5(file_path)
+            duplicates[file_hash].append(file_path)
 
     return duplicates
 
