99f6a7c48df49416ce487f8a2fab1119ad5577ed
Darya <sharkovadarya@gmail.com>
2016-10-15 22:13:06 +0300

    Fix: CF for UnaryOperation, return values

--- Python/HW5/folder.py
+++ Python/HW5/folder.py
@@ -7,53 +7,55 @@ class ConstantFolder:
         return new_tree
 
     def process_list(self, lst):
-        for i in range(len(lst)):
-            lst[i] = self.visit(lst[i])
+        ret_lst = []
+        for action in lst:
+            ret_lst.append(self.visit(action))
+        return ret_lst
 
     def visit_number(self, num):
         return num
 
     def visit_binary_operation(self, bin_op):
-        bin_op.lhs = self.visit(bin_op.lhs)
-        bin_op.rhs = self.visit(bin_op.rhs)
+        lhs = self.visit(bin_op.lhs)
+        rhs = self.visit(bin_op.rhs)
 
-        if (bin_op.op == '*' and (isinstance(bin_op.lhs, model.Number) and bin_op.lhs.number == 0 and
-                                  isinstance(bin_op.rhs, model.Reference) or
-                                  isinstance(bin_op.rhs, model.Number) and bin_op.rhs.number == 0 and
-                                  isinstance(bin_op.lhs, model.Reference))) or \
-           (isinstance(bin_op.lhs, model.Reference) and isinstance(bin_op.rhs, model.Reference) and bin_op.op == '-' and
-               bin_op.lhs.name == bin_op.rhs.name):
+        if (bin_op.op == '*' and (isinstance(lhs, model.Number) and lhs.number == 0 and
+                                  isinstance(rhs, model.Reference) or
+                                  isinstance(rhs, model.Number) and rhs.number == 0 and
+                                  isinstance(lhs, model.Reference))) or \
+           (isinstance(lhs, model.Reference) and isinstance(rhs, model.Reference) and bin_op.op == '-' and
+               lhs.name == rhs.name):
             return model.Number(0)
-        if isinstance(bin_op.lhs, model.Number) and isinstance(bin_op.rhs, model.Number):
-            return model.Number(bin_op.operations[bin_op.op](bin_op.lhs.number, bin_op.rhs.number))
-        return bin_op
+        if isinstance(lhs, model.Number) and isinstance(rhs, model.Number):
+            return model.Number(bin_op.operations[bin_op.op](lhs.number, rhs.number))
+        return model.BinaryOperation(lhs, bin_op.op, rhs)
 
     def visit_unary_operation(self, un_op):
-        while not isinstance(un_op.expression, model.Number):
-            un_op.expression = self.visit(un_op.expression)
-        return model.Number(un_op.operations[un_op.op](un_op.expression.number))
+        ex = self.visit(un_op.expression)
+        if isinstance(ex, model.Number):
+            return model.Number(un_op.operations[un_op.op](ex.number))
+        return model.UnaryOperation(un_op.op, ex)
 
     def visit_reference(self, ref):
         return ref
 
     def visit_conditional(self, cond):
-        cond.condition = cond.condition.visit(self)
-
-        self.process_list(cond.if_true or [])
-        self.process_list(cond.if_false or [])
-        return cond
+        condition = cond.condition.visit(self)
+        if_true = self.process_list(cond.if_true or [])
+        if_false = self.process_list(cond.if_false or [])
+        return model.Conditional(condition, if_true, if_false)
 
     def visit_print(self, prnt):
-        return prnt.expression.visit(self)
+        return model.Print(prnt.expression.visit(self))
 
     def visit_read(self, rd):
         return rd
 
     def visit_function_definition(self, func_def):
-        self.process_list(func_def.function.body)
-        return func_def
+        body = self.process_list(func_def.function.body)
+        return model.FunctionDefinition(func_def.name, model.Function(func_def.function.args, body))
 
     def visit_function_call(self, func_call):
-        self.process_list(func_call.args)
-        func_call.fun_expr = func_call.fun_expr.visit(self)
-        return func_call
+        args = self.process_list(func_call.args)
+        fun_expr = func_call.fun_expr.visit(self)
+        return model.FunctionCall(fun_expr, args)
